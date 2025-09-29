91bbfbb9fe9ac6664f7c7bc13a770703f2c6481a
adilll0x <aadilkhan707361@gmail.com>
2025-09-29 17:40:23 +0530

    Background and card background color change

--- app/src/main/java/com/example/skysense/WeatherPage.kt
+++ app/src/main/java/com/example/skysense/WeatherPage.kt
@@ -1,9 +1,12 @@
 package com.example.skysense
 
+import android.R
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -17,6 +20,7 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -26,7 +30,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -38,55 +44,78 @@ import com.example.skysense.api.WeatherModel
 @Composable
 fun WeatherPage(viewModel: WeatherViewModel)
 {
+
     var city by remember {
         mutableStateOf("")
     }
     val weatherResult = viewModel.weatherResult.observeAsState()
-
-    Column(
-        modifier = Modifier.fillMaxWidth().padding(8.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
-
+    val keyboardController = LocalSoftwareKeyboardController.current
+    Surface(
+        modifier = Modifier.fillMaxSize()
     ) {
-        Row (
-            modifier = Modifier.fillMaxWidth().padding(8.dp),
-            verticalAlignment =    Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceEvenly
-        ){
-            OutlinedTextField(
-                modifier = Modifier.weight(1f),
-                value = city,
-                onValueChange = { city = it },
-                label = {
-                    Text(text=    "Search for any location")
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    brush = Brush.verticalGradient(
+                        colors = listOf(
+                            Color(0xFFFFDEAD), // Navajo White (Soft Peach)
+                            Color(0xFFFFF0F5), // Lavender Blush (Soft Pinkish)
+                            Color(0xFFFFFFFF)  // White
+                        )
+                    )
+                )
+                .padding(horizontal = 8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+
+        ) {
+            Spacer(modifier = Modifier.height(30.dp))
+
+            Row(
+                modifier = Modifier.fillMaxWidth().padding(8.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                OutlinedTextField(
+                    modifier = Modifier.weight(1f),
+                    value = city,
+                    onValueChange = { city = it },
+                    label = {
+                        Text(text = "Search for any location")
+                    }
+                )
+                IconButton(onClick = {
+                    viewModel.getData(city)
+                    keyboardController?.hide()
+
+                }) {
+                    Icon(
+                        imageVector = Icons.Default.Search,
+                        contentDescription = "Search for Any location"
+                    )
                 }
-            )
-            IconButton(onClick =  {
-                viewModel.getData(city)
 
-            }) {
-                Icon(imageVector = Icons.Default.Search,
-                    contentDescription = "Search for Any location")
             }
 
-        }
+            when (val result = weatherResult.value) {
+                is NetworkResponse.Error -> {
+                    Text(text = result.message)
+                }
 
-        when(val result = weatherResult.value) {
-            is NetworkResponse.Error ->{
-                Text(text=result.message)
+                NetworkResponse.Loading -> {
+                    CircularProgressIndicator()
+                }
+
+                is NetworkResponse.Success<*> -> {
+                    WeatherDetails(result.data as WeatherModel)
+                }
+
+                null -> {}
             }
-            NetworkResponse.Loading -> {
-                CircularProgressIndicator()
-            }
-            is NetworkResponse.Success<*> ->{
-                WeatherDetails(result.data as WeatherModel)
-            }
-            null -> {}
+
         }
 
     }
-
-
 }
 
 
@@ -178,6 +207,5 @@ fun WeatherKeyVal(key : String ,value : String){
         Text(text = value, fontSize = 24.sp, fontWeight = FontWeight.Bold)
         Text(text = key, fontSize = 16.sp, fontWeight = FontWeight.SemiBold, color = Color.Gray)
     }
-
 
 }
